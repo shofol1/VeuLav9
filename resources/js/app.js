dbebfd54d270415c11e1be4bd5e85a5dbe81c23f
shofol <18103156@iubat.edu>
2022-08-02 22:02:30 +0600

    vue and laravel 9 setup

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import "./bootstrap";
+import { createApp } from "vue";
+import App from "./layouts/app.vue";
+
+createApp(App).mount("#app");
---
+++ resources/js/layouts/app.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Nav from "../components/Nav/Nav.vue";
+</script>
+
+<template>
+    <Nav></Nav>
+</template>
